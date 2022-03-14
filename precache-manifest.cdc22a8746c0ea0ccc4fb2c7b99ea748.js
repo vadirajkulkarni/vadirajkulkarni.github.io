0c90bbe46f183c90293f81520f896115e21cc0ef
vadirajkulkarni <iamvadiraj1995@gmail.com>
2022-03-14 11:28:07 +0530

    Updates

---
+++ precache-manifest.cdc22a8746c0ea0ccc4fb2c7b99ea748.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "555ca0ab043f343a5b7f1dd6a3dcd63d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "efa7cc0162ebc9e1076f",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "bab168a50cf9f1fc6328",
+    "url": "/static/js/2.d39a5ff0.chunk.js"
+  },
+  {
+    "revision": "4401b54a7a1e9c66a5ad38214e75a34e",
+    "url": "/static/js/2.d39a5ff0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3be54f7aa782d4027f3d",
+    "url": "/static/js/3.58770b79.chunk.js"
+  },
+  {
+    "revision": "efa7cc0162ebc9e1076f",
+    "url": "/static/js/main.43aad56e.chunk.js"
+  },
+  {
+    "revision": "1fb8104abb98daeef781",
+    "url": "/static/js/runtime-main.f4295d4c.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  }
+]);
